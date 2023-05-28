747afc40d005bec91275e6abff612b329891b065
JarbasAi <jarbasai@mailfence.com>
2023-05-28 20:58:47 +0100

    modernize imports

--- __init__.py
+++ __init__.py
@@ -1,14 +1,10 @@
-from mycroft.skills.core import MycroftSkill, intent_file_handler
-from mycroft.audio import wait_while_speaking
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 from py_edamam import Edaman
 
 
-__author__ = 'jarbas'
-
-
-class NutrientsSkill(MycroftSkill):
-    def __init__(self):
-        super(NutrientsSkill, self).__init__()
+class NutrientsSkill(OVOSSkill):
+    def initialize(self):
         # free keys for the people
         if "recipes_appid" not in self.settings:
             self.settings["recipes_appid"] = 'eceecbfb'
@@ -22,11 +18,11 @@ class NutrientsSkill(MycroftSkill):
                 'cabec6b9addb1666e1365303e509f450'
 
         self.edamam = Edaman(nutrition_appid=self.settings["nutrition_appid"],
-                           nutrition_appkey=self.settings["nutrition_appkey"],
-                           recipes_appid=self.settings["recipes_appid"],
-                           recipes_appkey=self.settings["recipes_appkey"])
+                             nutrition_appkey=self.settings["nutrition_appkey"],
+                             recipes_appid=self.settings["recipes_appid"],
+                             recipes_appkey=self.settings["recipes_appkey"])
 
-    @intent_file_handler("ingredients.intent")
+    @intent_handler("ingredients.intent")
     def handle_ingredients_intent(self, message):
         sentence = message.data["sentence"]
         # TODO use dialog file
@@ -36,12 +32,11 @@ class NutrientsSkill(MycroftSkill):
             if idx >= 2:
                 self.enclosure.deactivate_mouth_events()
                 self.enclosure.mouth_text(s)
-            self.speak(s)
-            wait_while_speaking()
+            self.speak(s, wait=True)
 
         self.enclosure.activate_mouth_events()
 
-    @intent_file_handler("calories.intent")
+    @intent_handler("calories.intent")
     def handle_calories_intent(self, message):
         sentence = message.data["sentence"]
         n = self.edamam.search_nutrient(sentence)
@@ -55,6 +50,3 @@ class NutrientsSkill(MycroftSkill):
         else:
             # TODO dialog file
             self.speak("unknown food")
-
-def create_skill():
-    return NutrientsSkill()
